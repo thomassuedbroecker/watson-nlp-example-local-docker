9ac72ca4f2b55a7bb7411e997a303f650827d56f
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2023-02-02 16:46:31 +0100

    add multistage file and automation

---
+++ code/Multistage.Dockerfile
@@ -0,0 +1,26 @@
+# Using the Docker container image for model
+
+ARG WATSON_RUNTIME_BASE
+ARG PRETRAINED_MODEL="cp.icr.io/cp/ai/watson-nlp_sentiment_aggregated-cnn-workflow_lang_en_stock:1.0.6"
+ARG CUSTOM_MODEL="us.icr.io/custom-watson-nlp-tsued/watson-nlp_ensemble_model:1.0.0"
+
+# ****************************
+# BUILD: Prepare and unpacked models inside a container
+# ****************************
+FROM ${PRETRAINED_MODEL} as pretrained
+RUN ./unpack_model.sh
+
+FROM ${CUSTOM_MODEL} as custom
+RUN ./unpack_model.sh
+
+# ****************************
+# PRODUCTION: Runtime with unpacked models
+# ****************************
+FROM ${WATSON_RUNTIME_BASE} as release
+
+RUN true && \
+    mkdir -p /app/models
+
+ENV LOCAL_MODELS_DIR=/app/models
+COPY --from=pretrained app/models /app/models
+COPY --from=custom app/models /app/models
---
+++ code/run-watson-nlp-with-dockerfile-multistage.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# **************** Global variables
+source ./.env
+
+# Information
+IMAGE_REGISTRY="cp.icr.io/cp/ai"
+RUNTIME_IMAGE="watson-nlp-runtime"
+WATSON_NLP_TAG="1.0.20"
+export WATSON_RUNTIME_BASE="$IMAGE_REGISTRY/$RUNTIME_IMAGE:$WATSON_NLP_TAG"
+RUNTIME_CONTAINER_NAME=watson-nlp-with-custom-models
+MODEL_CONTAINERS_NAME=watson-nlp-custom-models
+TEMP_MODEL_DIR=models
+
+######### Create custom Watson NLP image ##############
+CUSTOM_WATSON_NLP_IMAGE_NAME=watson-nlp-runtime-with-custom-models
+CUSTOM_TAG=1.0.0
+
+# **********************************************************************************
+# Functions definition
+# **********************************************************************************
+
+function connectToIBMContainerRegistry () {
+    echo ""
+    echo "# ******"
+    echo "# Connect to IBM Cloud Container Image Registry: $IMAGE_REGISTRY"
+    echo "# ******"
+    echo ""
+    echo "IBM_ENTITLEMENT_KEY: $IBM_ENTITLEMENT_KEY"
+    echo ""
+    docker login cp.icr.io --username cp --password $CONTAINER_ENTITLEMENT_KEY
+}
+
+function createCustomContainerImageLocally () {
+    echo ""
+    echo "# ******"
+    echo "# Create container image"
+    echo "# Runtime container image: $WATSON_RUNTIME_BASE"
+    echo "# ******"
+    echo ""
+    echo "Image name: $CUSTOM_WATSON_NLP_IMAGE_NAME"
+    docker build --build-arg WATSON_RUNTIME_BASE="$WATSON_RUNTIME_BASE" ./Multistage.Dockerfile -t "$CUSTOM_WATSON_NLP_IMAGE_NAME":"$CUSTOM_TAG"
+}
+
+function runNLPwithCustomModels () {
+
+    echo ""
+    echo "# ******"
+    echo "# Run NLP with custom model"
+    echo "# ******"
+    echo ""  
+    echo "# Run the custom runtime with the custom models mounted"
+    echo "# Image: $IMAGE_REGISTRY/$RUNTIME_IMAGE"
+    echo "" 
+    docker run --rm -it \
+      -e ACCEPT_LICENSE=true \
+      -p 8085:8085 \
+      -p 8080:8080 \
+      --name $RUNTIME_CONTAINER_NAME \
+      $CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+}
+
+#**********************************************************************************
+# Execution
+# *********************************************************************************
+
+connectToIBMContainerRegistry 
+
+createCustomContainerImageLocally
+
+runNLPwithCustomModels
